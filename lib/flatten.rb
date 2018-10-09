46a75fdd559e2f28890047071653dc6559c5397d
Ron Craig <ron.craig@comcast.net>
2018-10-08 17:14:03 -0700

    flatten_array spec is passing :tada:

--- lib/flatten.rb
+++ lib/flatten.rb
@@ -1,23 +1,28 @@
-require('rspec')
 require('pry')
 require('flatten')
 
-# Anagrams class and compare_words method
+# Flatten class and flatten_array method
 class Flatten
-  def initialize(array1)
-    @array1 = array1
+  def initialize(array)
+    @array = array
   end
 
-  def flats
-    original = @array1.to_s
-    new_array = original.split
+  def flatten_array
+    original = @array.to_s
+    middle = original[1..-2]
+    new_array = middle.split(//)
+    x = 0
     final_array = []
-    new_array.map do |i|
-      if i.include? ','
-        final_array.push('')
+    while x < new_array.count
+      arr = new_array[x]
+      if (arr == '[') | (arr == ']') | (arr == ' ') | (arr == ',')
+        final_array.push(nil)
       else
-        final_array.push(i)
+        arr = arr.to_i
+        final_array.push(arr)
       end
+      x += 1
     end
+    final_array.compact!
   end
 end
--- spec/flatten_spec.rb
+++ spec/flatten_spec.rb
@@ -1,12 +1,11 @@
 require('rspec')
-require('pry')
 require('flatten')
 
 describe(Flatten) do
-  describe('flats') do
-    it('removes extra [ & ] characters to create a single array') do
-      array = Flatten.new([1, 2, [3], [4, 5]])
-      expect(array.flats).to(eq([1, 2, 3, 4, 5]))
+  describe('flatten_array') do
+    it('removes "[" and "]" subarray characters to create a single array') do
+      array = Flatten.new([[1, 2, [3]], 4])
+      expect(array.flatten_array).to(eq([1, 2, 3, 4]))
     end
   end
 end
